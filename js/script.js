aa799acf4d395a55c14833c1f063b27452292d1a
tmlaDesign <tmlaDesign@gmail.com>
2023-11-17 14:07:42 -0500

    1st Draft Quote Generator
    quote generator is working but next steps are to refactor code, use loops, and meet the requirements of exceeds

--- js/script.js
+++ js/script.js
@@ -13,75 +13,57 @@ project 1 - A Random Quote Generator
 const quotes = [
   {
 quote: 'Feel the rage. The powerful, pure rage of not being able to forgive will become your unswerving drive to take action.',
-name: 'Giyu',
-source:'Demon Slayer',
-season:'Season 1',
-episode:'Episode 1',
-movie: ''
+source: 'Giyu',
+citation:'Demon Slayer',
+year:'Season 1'
 },
 {
   quote: 'No matter how many people you may lose, you have no choice but to go on living -- no matter how devastating the blows, maybe.',
-  name: 'Tengen',
-  source:'Demon Slayer',
-  season:'',
-  episode:'',
-  movie: 'The Entertainment District Movie'
+  source: 'Tengen',
+  citation:'Demon Slayer',
+  year:'Season 2'
   },
   {
     quote: 'It\'s okay to do things a little differently.',
-    name: 'Tanjiro',
-    source:'Demon Slayer',
-    season:'Season 1',
-    episode:'Episode 7',
-    movie: ''
+    source: 'Tanjiro',
+    citation:'Demon Slayer',
+    year:'Season 3'
     },
     {
       quote: 'Those who regretted their own actions, I would never trample over them. Because demons were once human too. Just like me, they were human too.',
-      name: 'Tanjiro',
-      source:'Demon Slayer',
-      season:'Season 1',
-      episode:'Episode 2',
-      movie: ''
+      source: 'Tanjiro',
+      citation:'Demon Slayer',
+      year:'Season 4'
       },
       {
         quote: 'Don\'t ever give up. Even if it\'s painful, even if it\'s agonizing, don\'t try to take the easy way out.',
-        name: 'Zenitsu',
-        source:'Demon Slayer',
-        season:'Season 1',
-        episode:'Episode 17',
-        movie: ''
+        source: 'Zenitsu',
+        citation:'Demon Slayer',
+        year:'Season 5'
         },
         {
           quote: 'All I can do is work hard! That\'s the story of my life!',
-          name: 'Tanjiro',
-          source:'Demon Slayer',
-          season:'Season 1',
-          episode:'Episode 2',
-          movie: ''
+          source: 'Tanjiro',
+          citation:'Demon Slayer',
+          year:'Season 1'
           },
           {
             quote: 'If you are feeling disheartened, that you are somehow not enough, set your heart ablaze.',
-            name: 'Tanjiro',
-            source:'Demon Slayer',
-            season:'Season 2',
-            episode:'Episode 7',
-            movie: ''
+            source: 'Tanjiro',
+            citation:'Demon Slayer',
+            year:'Season 2'
             },
             {
               quote: 'If you can do one thing, hone it to perfection. Hone it to the utmost limit.',
-              name: 'Jigoro',
-              source:'Demon Slayer',
-              season:'Season 1',
-              episode:'Episode 16',
-              movie: ''
+              source: 'Jigoro',
+              citation:'Demon Slayer',
+              year:'Season 3'
               },
               {
                 quote: 'I can do it. I know I can do it. I\'m the guy who gets it done, broken bones or not. No matter what, I can do it! I can fight!',
-                name: 'Tanjiro',
-                source:'Demon Slayer',
-                season:'Season 1',
-                episode:'Episode 13',
-                movie: ''
+                source: 'Tanjiro',
+                citation:'Demon Slayer',
+                year:'Season 4'
                 }
 
 ]
@@ -93,18 +75,51 @@ movie: ''
 
 function getRandomQuote(array) {
   
-      let quoteIndex = (Math.floor(Math.random() * quotes.length) +1);
+      let quoteIndex = (Math.floor(Math.random() * quotes.length));
       console.log(quoteIndex);
-      return quoteIndex;
+    
+     let chosenQuote = (array[quoteIndex].quote);
+     let chosenSource = (array[quoteIndex].source);
+     let chosenCitation = (array[quoteIndex].citation);
+     let chosenYear = (array[quoteIndex].year);
      
-  
+     
+     
+let chosenQuoteObject = {
+  quote: chosenQuote,
+  source: chosenSource,
+  citation: chosenCitation,
+  year: chosenYear
 }
 
-getRandomQuote(quotes);
+console.log(chosenQuoteObject)
+
+return chosenQuoteObject;
+}
+
+
+
 
 /***
  * `printQuote` function
 ***/
+
+function printQuote (func) {
+let printQuote = document.querySelector('p.quote')
+let printSource = document.querySelector('.source')
+let printCitation = document.querySelector('.citation')
+let printYear = document.querySelector('.year')
+
+ printQuote.innerHTML = `${getRandomQuote(quotes).quote}`
+ printSource.innerHTML = `${getRandomQuote(quotes).source}, <i>${getRandomQuote(quotes).citation}</i>, ${getRandomQuote(quotes).year}`
+//  printCitation.innerHTML = ``
+//  printYear.innerHTML = ``
+}
+
+
+printQuote(getRandomQuote(quotes));
+
+
 
 
 
@@ -113,4 +128,4 @@ getRandomQuote(quotes);
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
-// document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
